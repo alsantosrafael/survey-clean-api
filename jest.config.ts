4303d788192abd9d2573c0cc738d8f70cb47f6df
Rafael Almeida <rafael.profeng@gmail.com>
2022-12-01 15:36:52 -0300

    refactor: move duplicated code to a helper method

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@
 export default {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverage: true,
+  // collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
